21ee780df5c3f805d0554152b83ff2a4537cd95e
Nithin-kasturi <nithinkasturi8@gmail.com>
2026-06-27 21:39:12 +0530

    first commit

---
+++ my-app/src/app/components/roles/roles.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  imports: [],
+  templateUrl: './roles.html',
+  styleUrl: './roles.css',
+})
+export class Roles {
+
+  firstName:string="Kasturi";
+  lastName:string="Nithin"
+  
+}
